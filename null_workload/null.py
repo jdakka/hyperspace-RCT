3e561fee2ec705541cc0418c16785c872303f30b
Jumana Dakka <dakka@br006.pvt.bridges.psc.edu>
2018-09-07 17:51:23 -0400

    fixed bugs

--- null_workload/null.py
+++ null_workload/null.py
@@ -19,21 +19,15 @@ class HyperSpaceTask(Task):
         self.arguments = ['100']
         # self.arguments = ['-c', '24', '-t', '600']
         # self.arguments = ['.sh file']
-        self.parameters = parameter
-        self.cpu_reqs =  {
-        
-                        'processes': 24,
-                        'process_type': None/MPI,
-                        'threads_per_process': 1,
-                        'thread_type': None/OpenMP
-                    }
+        # self.parameters = parameter
+      	self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 24, 'process_type': None}
 
 
 if __name__ == '__main__':
 
     # arguments for AppManager
-    total_cores = sys.argv[1] # 2**H*24 
-    duration = sys.argv[2] 
+    total_cores = int(sys.argv[1]) # 2**H*24 
+    duration = int(sys.argv[2]) 
 
 
     # define the global search space bounds for each search dimension
@@ -47,31 +41,36 @@ if __name__ == '__main__':
     pipelines = set()
     p = Pipeline()
     s = Stage() # 1 stage of bag-of-tasks (2**H tasks)
+   
 
-    for i in (len(hparams)**2): # for each hyperspace
+    for i in range((len(hparams)**2)): # for each hyperspace
     
-        t = HyperSpaceTask(name = 'optimization_{}'.format(i), parameter = i) 
+        # t = HyperSpaceTask(name = 'optimization_{}'.format(i), parameter = i) 
         # run Bayesian optimization for N-iterations in parallel
-        s.add_tasks(t)
+        t = Task()
+	t.name = 'optimization_{}'.format(i)
+        t.executable = ['/bin/sleep']
+        t.arguments = ['100']
+        # self.arguments = ['-c', '24', '-t', '600']
+        # self.arguments = ['.sh file']
+        # t.parameters = parameter
+        t.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 24, 'process_type': None}
 
-    p.add_stage(s)
+	s.add_tasks(t)
+
+    p.add_stages(s)
     pipelines.add(p)
 
     # Resource and AppManager
-
+    amgr = AppManager(hostname = 'two.radical-project.org', port = 33048)
     amgr.workflow = pipelines
     amgr.shared_data = []
-
-
-    amgr = AppManager()
-    amgr.resource_desc = {
-        'resource': 'xsede.bridges',
-        'walltime': duration,
-        'cpus': total_cores,
-        'rmq_cleanup': true,
-        'autoterminate': true,
-        'port' : 33048,
-        'hostname': 'localhost'}
-
-
    
+    amgr.resource_desc = {
+        'resource': 'local.localhost',
+        'walltime': duration,
+        'cpus': total_cores}
+       
+
+
+    amgr.run()
