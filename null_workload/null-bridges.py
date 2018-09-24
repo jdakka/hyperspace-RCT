41d24bef19b73d914f7bedc787b08ace4ac0df6b
Jumana Dakka <jumanadakka@gmail.com>
2018-09-24 17:12:11 -0400

    latest

--- null_workload/null-bridges.py
+++ null_workload/null-bridges.py
@@ -3,7 +3,10 @@ import os
 import traceback
 import sys
 import pickle # change the pickle to json load and dump 
+from glob import glob
 import radical.utils as ru
+import shutil
+
 
 # convert tabs to spaces (no tabs)
 
@@ -13,6 +16,10 @@ import radical.utils as ru
 
 if os.environ.get('RADICAL_ENTK_VERBOSE') == None:
     os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+hostname = os.environ.get('RMQ_HOSTNAME','localhost')
+port = int(os.environ.get('RMQ_PORT',5672))
 
 logger = ru.Logger(__name__, level='DEBUG')
 
@@ -36,11 +43,11 @@ class HyperSpaceTask(Task):
         # hyperspaces 
 
         super(HyperSpaceTask, self).__init__()
-        self.name = name
-        self.pre_exec = ['source activate ve_hyperspace']
+        self.name = name  
+        self.pre_exec = ['source activate ve_hyperspace'] 
+        self.executable = ['/bin/sleep']
+        self.arguments = ['5']  
         self.copy_input_data = ['$SHARED/hyperspaces.py']
-        self.executable = ['python']
-        self.arguments = ['hyperspaces.py', 'hyperparameters']
         self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 1, 'process_type': None}
 
 
@@ -50,76 +57,101 @@ class OptimizationStage(Stage):
         self.name = name    
 
 class OptimizationTask(Task):
-    def __init__(self, name, hyperspace_index):
+    def __init__(self, name, hyperspace_index, duration):
 
         # this task will execute a Bayesian optimization
         # each task takes a unique hyperspace input  
         
         super(OptimizationTask, self).__init__()
         self.name = name
-        self.copy_input_data = ['/home/dakka/spaces.txt']
-        self.copy_input_data += ['$SHARED/hyperspaces.py']
+        self.copy_input_data = ['$SHARED/optimization.py']
         self.pre_exec = ['export PATH=/home/dakka/stress-ng-0.09.40:$PATH']
         # self.pre_exec += ['python optimization.py {}'.format(hyperspace_index)]
         self.executable = ['stress-ng'] 
-        self.arguments = ['-c', '24', '-t', '6000']
-        self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 24, 'process_type': None}
-
+        self.arguments = ['-c', '28', '-t', '{}'.format(duration)]
+        self.cpu_reqs = {'processes': 1, 'thread_type': None, 'threads_per_process': 28, 'process_type': None}
 
 
 if __name__ == '__main__':
 
     # arguments for AppManager
 
-    walltime = int(sys.argv[1]) 
 
+    stress_ng_duration = int(sys.argv[1]) 
+
+    number_of_hyperparameters = int(sys.argv[2])
+
+    walltime = int(sys.argv[3])
 
     # user defines the global search space bounds for each search dimension
 
-    hparams = [(0,7), (10,17)] 
+    initial_hparams = [(0,7)] 
+
+    final_hparams = list()
+
+    final_hparams += number_of_hyperparameters * [initial_hparams[0]]
+
 
     # EnTK single pipeline of two stages 
 
-    pipelines = set()
-    p = HyperSpacePipeline(name = 'hyperspace_pipeline')
-
+    # pipelines = set()
     
     # Stage 1: generate all combinations of search subspaces (hyperspaces)
 
-    s = HyperSpaceStage(name = 'generate_hyperspaces_stage')
-    t = HyperSpaceTask(name = 'generate_hyperspace_task', hyperparameters = hparams)
-    s.add_tasks(t) 
-    p.add_stages(s)
+    p = HyperSpacePipeline(name = 'hyperspace_pipeline')
 
-    logger.info('adding stage {} with {} tasks'.format(s.name, s._task_count))
+    s1 = HyperSpaceStage(name = 'hyperspace_stage' )
+    
+    t1 = HyperSpaceTask(name = 'hyperspace_generator_task', hyperparameters = final_hparams)  
+        
+    # Add the Task to the Stage
+    s1.add_tasks(t1)
+
+    # Add Stage to the Pipeline
+    p.add_stages(s1)
+
+    logger.info('adding stage {} with {} tasks'.format(s1.name, s1._task_count))
 
     # Stage 2: bag-of-tasks for Bayesian optimizations (2**H tasks) 
 
-    s = OptimizationStage(name = 'optimizations') 
-    for i in range(len(hparams)**2): 
+    s2 = OptimizationStage(name = 'optimizations') 
+    for i in range(2**len(final_hparams)): 
     
         # run Bayesian optimization in parallel
         # each optimization runs for n_iterations
 
-        t = OptimizationTask(name = 'optimization_{}'.format(i), hyperspace_index = i)
-        s.add_tasks(t)
+        t = OptimizationTask(name = 'optimization_{}'.format(i), hyperspace_index = i,
+                            duration = stress_ng_duration)
+        s2.add_tasks(t)
         
-    p.add_stages(s)
-    pipelines.add(p)
+    p.add_stages(s2)
+        
 
-    logger.info('adding stage {} with {} tasks'.format(s.name, s._task_count))
-    logger.info('adding pipeline {} with {} stages'.format(p.name, p._stage_count))
+    # logger.info('adding stage {} with {} tasks'.format(s.name, s._task_count))
+    # logger.info('adding pipeline {} with {} stages'.format(p.name, p._stage_count))
 
-    # Resource and AppManager
-    amgr = AppManager(hostname = 'two.radical-project.org', port = 33048)
-    amgr.workflow = pipelines
-    amgr.shared_data = ['hyperspaces.py','optimization.py']
-    amgr.resource_desc = {
+    # Create Application Manager
+    appman = AppManager(hostname=hostname, port=port)
+
+    res_dict = {
+
         'resource': 'xsede.bridges',
         'project' : 'mc3bggp',
         'queue' : 'RM',
         'walltime': walltime,
-        'cpus': len(hparams)**2*24,
-        'access_schema': 'gsissh'}
-       
-    amgr.run()
+        'cpus': 2**len(final_hparams)*28,
+        'access_schema': 'gsissh'
+    }
+
+
+    # Assign resource manager to the Application Manager
+    appman.resource_desc = res_dict
+    appman.shared_data = ['%s/binaries/hyperspaces.py' %cur_dir, '%s/binaries/optimization.py' %cur_dir]
+
+    
+    # Assign the workflow as a set of Pipelines to the Application Manager
+    appman.workflow = [p]
+
+    # Run the Application Manager
+    appman.run()
+
